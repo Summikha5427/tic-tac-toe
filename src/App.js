bdc90a0a812ee6ee006a4df3b1fd0343e3b56449
Summikha5427 <summikhan5426@gmail.com>
2024-01-16 13:02:44 -0600

    first commit to ttt website-trial

--- src/App.js
+++ src/App.js
@@ -1,95 +1,96 @@
-//Tic-Tac-Toe (React-compoents with redux )
+// Import necessary libraries and functions
+import React, { useEffect } from "react";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import back from "./Images/back.jpg"
 
-import { useEffect} from "react";
-import { Provider, connect } from "react-redux"; //helps in integrating React Components with redux store
-import { createStore } from "redux"; // helps o creating store to use redux with react-components
-// helps in avoiding the use of props for passing the data from component  to another
 
-// REDUX: Initial State
+// Action Types
+const SET_MARKS = "set_Marks";
+const SET_PLAYER = "set_Player";
+const SET_GAME_OVER = "set_GameOver";
+const SET_WINNING_CELLS = "set_WinningCells";
+
+// Initial Redux State
 const initialState = {
   marks: [0, 0, 0, 0, 0, 0, 0, 0, 0],
   player: 1,
-  gameOver: false
+  gameOver: false,
+  winningCells: [],
 };
 
-//REDUX: Reducer
+// Reducer Function
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "set_Marks":
+    case SET_MARKS:
       return { ...state, marks: action.payload };
-    case "set_Player":
+    case SET_PLAYER:
       return { ...state, player: action.payload };
-    case "set_GameOver":
+    case SET_GAME_OVER:
       return { ...state, gameOver: action.payload };
+    case SET_WINNING_CELLS:
+      return { ...state, winningCells: action.payload };
     default:
       return state;
   }
 };
 
-//Redux Store
+// Redux Store
 const store = createStore(reducer);
 
 // App Component with Provider on BoardContainer
 function App() {
   return (
-    <div className="App">
-      <div>
+    <div className="App h-screen flex justify-center items-center bg-red-300 bg-cover" style={{backgroundImage:`url(${back})`}} >
+      <div >
         <Provider store={store}>
-          <BoardContainer></BoardContainer>
+          <BoardContainer />
         </Provider>
       </div>
     </div>
   );
 }
 
-//two arguments with the connection to the component which is stored in the container
-
-// ---simple state define .Here we have only two props : marks and players
-
-//-----BoardContainer Component using Connect and Map functions
-
+// BoardContainer Component using Connect and Map functions
 const mapStateToProps = (state) => {
   return {
     marks: state.marks,
     player: state.player,
-    gameOver: state.gameOver
+    gameOver: state.gameOver,
+    winningCells: state.winningCells,
   };
 };
 
-//---dispatch contains fucntions to the props
-const mapDisptachToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
   return {
     setMarks: (marks) => {
-      dispatch({ type: "set_Marks", payload: marks });
+      dispatch({ type: SET_MARKS, payload: marks });
     },
     setPlayer: (player) => {
-      dispatch({ type: "set_Player", payload: player });
+      dispatch({ type: SET_PLAYER, payload: player });
     },
     setGameOver: (status) => {
-      dispatch({ type: "set_GameOver", payload: status });
+      dispatch({ type: SET_GAME_OVER, payload: status });
+    },
+    setWinningCells: (cells) => {
+      dispatch({ type: SET_WINNING_CELLS, payload: cells });
     },
   };
 };
 
-//requires a container to use the props from the component.
-// A container which connects the component with th estate and its action and also to use the props of the components.
-// The container consists of two argument .
-// one for 'state' which consists of simple states of the app
-// second fo for the dispatch action which will be a function.
+const BoardContainer = connect(mapStateToProps, mapDispatchToProps)(Board);
 
-const BoardContainer = connect(mapStateToProps, mapDisptachToProps)(Board);
-
-//Main Functional Component (Parent)
-
-function Board({ marks, setMarks, player, setPlayer, gameOver, setGameOver }) {
-  // passing the variable as props to store
-  // Removing hooks as it more easier with redux
-
-  // const [marks, setMarks] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  // const [player, setPlayer] = useState(1);
-
-  //useEffect is always at the top  after the decalration of variables or the useState
-
+// Main Functional Component (Parent)
+function Board({
+  marks,
+  setMarks,
+  player,
+  setPlayer,
+  gameOver,
+  setGameOver,
+  winningCells,
+  setWinningCells,
+}) {
   useEffect(() => {
     const combination = [
       [0, 1, 2],
@@ -103,62 +104,110 @@ function Board({ marks, setMarks, player, setPlayer, gameOver, setGameOver }) {
     ];
 
     for (let c of combination) {
-      if (marks[c[0]] === 1 && marks[c[1]] === 1 && marks[c[2]] === 1) {
-        console.log(`player 1 wins, GameOver.`);
+      if (
+        marks[c[0]] === 1 &&
+        marks[c[1]] === 1 &&
+        marks[c[2]] === 1
+      ) {
         setGameOver(true);
+        setWinningCells(c);
+        alert(`Player 1 wins.GameOver`);
       }
-      if (marks[c[0]] === 2 && marks[c[1]] === 2 && marks[c[2]] === 2) {
-        console.log(`player 2 wins, GameOver.`);
+      if (
+        marks[c[0]] === 2 &&
+        marks[c[1]] === 2 &&
+        marks[c[2]] === 2
+      ) {
         setGameOver(true);
+        setWinningCells(c);
+        alert(`Player 2 wins.GameOver`);
       }
     }
-  }, [marks]);
+  }, [marks, setGameOver, setWinningCells]);
 
   const changeMark = (i) => {
-    const m = [...marks]; //destructuting - making a copy of the array to use the change of state
+    const m = [...marks];
 
     if (m[i] === 0 && !gameOver) {
       m[i] = player;
       setMarks(m);
       setPlayer(player === 1 ? 2 : 1);
     } else {
-      alert(` GameOver.`);
+      alert(`GameOver.`);
     }
   };
 
   return (
     <div className="Board grid  place-content-center ">
       <div>
-        <Block mark={marks[0]} position={0} changeMark={changeMark}></Block>
-        <Block mark={marks[1]} position={1} changeMark={changeMark}></Block>
-        <Block mark={marks[2]} position={2} changeMark={changeMark}></Block>
+        <Block
+          mark={marks[0]}
+          position={0}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(0)}
+        />
+        <Block
+          mark={marks[1]}
+          position={1}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(1)}
+        />
+        <Block
+          mark={marks[2]}
+          position={2}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(2)}
+        />
       </div>
       <div>
-        <Block mark={marks[3]} position={3} changeMark={changeMark}></Block>
-        <Block mark={marks[4]} position={4} changeMark={changeMark}></Block>
-        <Block mark={marks[5]} position={5} changeMark={changeMark}></Block>
+        <Block
+          mark={marks[3]}
+          position={3}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(3)}
+        />
+        <Block
+          mark={marks[4]}
+          position={4}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(4)}
+        />
+        <Block
+          mark={marks[5]}
+          position={5}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(5)}
+        />
       </div>
       <div>
-        <Block mark={marks[6]} position={6} changeMark={changeMark}></Block>
-        <Block mark={marks[7]} position={7} changeMark={changeMark}></Block>
-        <Block mark={marks[8]} position={8} changeMark={changeMark}></Block>
+        <Block
+          mark={marks[6]}
+          position={6}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(6)}
+        />
+        <Block
+          mark={marks[7]}
+          position={7}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(7)}
+        />
+        <Block
+          mark={marks[8]}
+          position={8}
+          changeMark={changeMark}
+          isWinning={winningCells.includes(8)}
+        />
       </div>
     </div>
   );
 }
 
-//Child Component - Passing data to parent compoment using redux store
-// Props gets stored in the store which is connected with "connect"
-//the "Provider" makes the store avaialble for the components.
-
-// A container helps with the connection to the Funcitonal component which in turns
-// takes two paprameters (mapstatetoprops, mapdispatchtoprops)
-
-//----- Block Component Child
-function Block({ mark, changeMark, position }) {
+// Block Component Child
+function Block({ mark, changeMark, position, isWinning }) {
   return (
     <div
-      className={`Block m${mark}`}
+      className={`Block m${mark} ${isWinning ? "winningCells" : ""}`}
       onClick={(e) => changeMark(position)}
     ></div>
   );
